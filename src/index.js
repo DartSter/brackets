4e1b797a9bc5c6d8c180c8ed2ebdbf09d1925627
DartSter <kil-all@rambler.ru>
2023-02-12 23:49:35 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let slicedStr = str;
+  const reg = bracketsConfig.map((item) => {
+    return `${item[0].match(/[\[\]\{\}\(\)\|]/) ? `\\${item[0]}` : item[0]}${
+      item[1].match(/[\[\]\{\}\(\)\|]/) ? `\\${item[1]}` : item[1]
+    }`;
+  }, "");
+
+  for (let i = 0; i < str.length; i++) {
+    reg.forEach(
+      (i) => (slicedStr = slicedStr.replace(new RegExp(`${i}`, "g"), ""))
+    );
+  }
+  console.log(!slicedStr.length);
+  return !slicedStr.length;
 }
